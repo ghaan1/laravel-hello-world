80e236595fba014ee3c8ee1f7db9df108d7319b2
ghaan1 <gono0608@gmail.com>
2022-02-23 15:23:25 +0700

    Pertemuan 2 , Prak 3

--- routes/web.php
+++ routes/web.php
@@ -16,9 +16,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    echo "Hi! Selamat Datang di Website Laravel " ;
-});
+
+// Route::get('/', function () {
+//     echo "Hi! Selamat Datang di Website Laravel ";
+// });
 
 Route::get('/about', function () {
     echo "2041720178 <br> Muhammad Ghaniyu Haq Haryanto <br> TI - 2G";
@@ -32,3 +33,61 @@ Route::get('/articles/{id}', function ($id) {
 Route::get('/', [HomeController::class, 'index']);
 Route::get('/about', [AboutController::class, 'about']);
 Route::get('/articles/{id}', [ArticleController::class, 'articles']);
+
+//prak3 
+Route::get('/', function () {
+    echo "Ini contoh dari 'Home'";
+});
+
+Route::get('/category', function () {
+    echo "Ini contoh dari 'Category'";
+    echo "Marbel Edu Game<br>";
+    echo "Marbel and Friend Kids Games<br>";
+    echo "Riri Story Books<br>";
+    echo "Kolak Kids Songs<br>";
+});
+
+    Route::prefix('category') -> group(function(){
+        Route::get("/marbel-edu-games", function(){
+            echo "Ini contoh dari 'Marbel Edu Game'";
+        });
+        Route::get("/marbel-and-friends-kids-games", function(){
+            echo "Ini contoh dari 'Marbel and Friend Kids Games'";
+        });
+        Route::get("/riri-story-books", function(){
+            echo "Ini contoh dari 'Riri Story Books'";
+        });
+        Route::get("/kolak-kids-songs", function(){
+            echo "Ini contoh dari 'Kolak Kids Songs'";
+        });
+    });
+
+Route::get('/news', function(){
+    echo "Ini contoh dari 'News'";
+});
+
+    Route::prefix('news') -> group(function(){
+        Route::get("/educa-studio-berbagi-untuk-warga-sekitar-terdampak-covid-19", function(){
+            echo "Ini contoh dari EducaStudio berbagi untuk warga sekitar terdampak Covid-19";
+        });
+    });
+
+Route::get('/program', function(){
+    echo "Ini contoh dari 'Program'";
+});
+
+    Route::prefix('program') -> group(function(){
+        Route::get("/karir" , function(){
+            echo "Ini contoh dari SubMenu Karir";
+        });
+        Route::get("/magang" , function(){
+            echo "Ini contoh dari SubMenu Magang";
+        });
+        Route::get("/kunjungan-industri" , function(){
+            echo "Ini contoh dari SubMenu Industri";
+        });
+    });
+
+Route::get("/about-us" , function(){
+    echo " Ini contoh dari 'About Us' <br> Muhammad Ghaniyu Haq Haryanto <br> 2041720178 <br> TI - 2G";
+});
