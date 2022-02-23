8c75ef1270b5085ab63ac7de2c401b6cca118c68
ghaan1 <gono0608@gmail.com>
2022-02-23 13:31:22 +0700

    Pertemuan 2, Prak 2

---
+++ app/Http/Controllers/AboutController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AboutController extends Controller
+{
+    public function about(){
+        return "2041720178 <br> Muhammad Ghaniyu Haq Haryanto <br> TI - 2G <br> Baru";
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,4 +1,7 @@
 <?php
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\AboutController;
+use App\Http\Controllers\ArticleController;
 use App\Http\Controllers\PageController;
 use Illuminate\Support\Facades\Route;
 
@@ -26,6 +29,6 @@ Route::get('/articles/{id}', function ($id) {
 });
 
 //prak2
-Route::get('/', [PageController::class, 'index']);
-Route::get('/about', [PageController::class, 'about']);
-Route::get('/articles/{id}', [PageController::class, 'articles']);
+Route::get('/', [HomeController::class, 'index']);
+Route::get('/about', [AboutController::class, 'about']);
+Route::get('/articles/{id}', [ArticleController::class, 'articles']);
