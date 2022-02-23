989ddf58bc370fcf7b2d6db42aa5d200fce0bfbf
ghaan1 <gono0608@gmail.com>
2022-02-23 12:48:41 +0700

    Pertemuan 2

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,13 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    echo "Hi! Selamat Datang di Website Laravel" ;
+});
+
+Route::get('/about', function () {
+    echo "2041720178 <br> Muhammad Ghaniyu Haq Haryanto <br> TI - 2G";
+});
+
+Route::get('/articles/{id}', function ($id) {
+    echo "Ini adalah halaman Artikel dengan ID : $id";
 });
