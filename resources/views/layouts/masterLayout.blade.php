ea068ef49a80e81f82aafb2e2d97f7f971a7cfd6
ghaan1 <gono0608@gmail.com>
2022-03-23 15:47:09 +0700

    PERTEMUAN 6

--- resources/views/layouts/masterLayout.blade.php
+++ resources/views/layouts/masterLayout.blade.php
@@ -31,24 +31,36 @@
       <!--[if lt IE 9]>
       <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
       <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script><![endif]-->
+      <meta charset="utf-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+
+      <!-- CSRF Token -->
+      <meta name="csrf-token" content="{{ csrf_token() }}">
+
+      <title>{{ config('app.name', 'Laravel') }}</title>
+
+      <!-- Scripts -->
+      <script src="{{ asset('js/app.js') }}" defer></script>
+
+      <!-- Fonts -->
+      <link rel="dns-prefetch" href="//fonts.gstatic.com">
+      <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+
+      <!-- Styles -->
+      <link href="{{ asset('css/app.css') }}" rel="stylesheet">
    </head>
    <!-- body -->
-   <body class="main-layout">
-       <!-- loader  -->
-
-        <div class="loader_bg">
-         <div class="loader"><img src="{{url('assets/images/loading.gif')}}" alt="#" /></div>
-      </div> 
-
-      <!-- end loader>
-
-      
-        <!-- header -->
-        <header class="section">
-         <!-- header inner -->
-
-
-         <div class="header">
+   <div id="main_slider" class="section carousel slide banner-main" data-ride="carousel">
+            <ol class="carousel-indicators">
+               <li data-target="#main_slider" data-slide-to="0" class="active"></li>
+               <li data-target="#main_slider" data-slide-to="1"></li>
+               <li data-target="#main_slider" data-slide-to="2"></li>
+            </ol>
+            <body class="main-layout">
+               <!-- header -->
+               <header class="section">
+                  <!-- header inner -->
+         <div class="header" style="background-color: #136af8;">
             <div class="container">
                <div class="row">
                   <div class="col-xl-3 col-lg-3 col-md-3 col-sm-3 col logo_section">
@@ -63,6 +75,37 @@
                         <div class="limit-box">
                            <nav class="main-menu">
                               <ul class="menu-area-main">
+                              @guest
+                              @if (Route::has('login'))
+                                          <li class="nav-item">
+                                                <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                                          </li>
+                                       @endif
+                                       
+                                       @if (Route::has('register'))
+                                       <li class="nav-item">
+                                          <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                                          </li>
+                                          @endif
+                                          @else
+                                          <li class="nav-item dropdown">
+                                             <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                                {{ Auth::user()->name }}
+                                             </a>
+                                             
+                                          <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+                                                <a class="dropdown-item" href="{{ route('logout') }}"
+                                                   onclick="event.preventDefault();
+                                                               document.getElementById('logout-form').submit();">
+                                                   {{ __('Logout') }}
+                                                </a>
+
+                                                <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                                                   @csrf
+                                                </form>
+                                          </div>
+                                       </li>
+                                    @endguest
                                  <li> <a href="index.html">Home</a> </li>
                                  <li> <a href="about.html">About</a> </li>
                                  <li><a href="testmonial.html">Testmonial</a></li>
@@ -78,17 +121,16 @@
             </div>
          </div>
          <!-- end header inner -->
+      </div>
       </header>
       <!-- end header -->
+      @yield('content')
 
-      
-      
-       @yield('content')
-       <!-- footer start-->
+      <!-- footer start-->
       <div id="plant" class="footer layout_padding">
-         <div class="container">
-            <p>©️ 2019 All Rights Reserved. Design by<a href="https://html.design/"> Free Html Templates</a></p>
-         </div>
+          <div class="container">
+              <p>©️ 2019 All Rights Reserved. Design by<a href="https://html.design/"> Free Html Templates</a></p>
+          </div>
       </div>
-   </body>
+    </body>
 </html>
